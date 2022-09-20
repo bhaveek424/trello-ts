3c2421410b05469c109497764ff86c1081ecc6c2
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-09-21 01:00:27 +0530

    adding Lists

--- src/state/appStateReducer.ts
+++ src/state/appStateReducer.ts
@@ -1,4 +1,5 @@
 import { Action } from "./actions";
+import { nanoid } from "nanoid";
 
 export type Task = {
   id: string;
@@ -19,10 +20,16 @@ export const appStateReducer = (
   draft: AppState,
   action: Action
 ): AppState | void => {
-  switch (
-    action.type
-    // here we call the state a draft, because we are usign Immer and we'll mutate this object directly
-    //this way we remind ourselves that this is not a regular reducer state and we dont have to worry about the immutability
-  ) {
+  switch (action.type) {
+    case "ADD_LIST": {
+      draft.lists.push({
+        id: nanoid(),
+        text: action.payload,
+        tasks: [],
+      });
+      break;
+    }
   }
+  // here we call the state a draft, because we are usign Immer and we'll mutate this object directly
+  //this way we remind ourselves that this is not a regular reducer state and we dont have to worry about the immutability
 };
