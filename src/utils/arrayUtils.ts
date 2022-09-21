9ca3943dde1a85ddef695d17e517e1f934da80da
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-09-21 20:49:53 +0530

    implementing move list feature

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -8,3 +8,20 @@ export const findItemIndexById = <TItem extends Item>(
 ) => {
   return items.findIndex((item: TItem) => item.id === id);
 };
+
+export function removeItemAtIndex<TItem>(array: TItem[], index: number) {
+  return [...array.slice(0, index), ...array.slice(index + 1)];
+}
+
+export function insertItemAtIndex<TItem>(
+  array: TItem[],
+  item: TItem,
+  index: number
+) {
+  return [...array.slice(0, index), item, ...array.slice(index)];
+}
+
+export const moveItem = <TItem>(array: TItem[], from: number, to: number) => {
+  const item = array[from];
+  return insertItemAtIndex(removeItemAtIndex(array, from), item, to);
+};
