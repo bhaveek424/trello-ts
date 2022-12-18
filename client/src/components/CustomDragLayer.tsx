8ac5b3285c51ebda5852f67fbafb8b01a618b3a4
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-12-19 03:39:24 +0530

    backend to be implemented

---
+++ client/src/components/CustomDragLayer.tsx
@@ -0,0 +1,27 @@
+import { useDragLayer } from "react-dnd";
+import { useAppState } from "../state/AppStateContext";
+import { CustomDragLayerContainer } from "../styles";
+import { Card } from "./Card";
+import { Column } from "./Column";
+
+export const CustomDragLayer = () => {
+  const { draggedItem } = useAppState();
+  const { currentOffset } = useDragLayer((monitor) => ({
+    currentOffset: monitor.getSourceClientOffset(),
+  }));
+
+  return draggedItem && currentOffset ? (
+    <CustomDragLayerContainer>
+      {draggedItem.type === "COLUMN" ? (
+        <Column id={draggedItem.id} text={draggedItem.text} isPreview />
+      ) : (
+        <Card
+          columnId={draggedItem.columnId}
+          isPreview
+          id={draggedItem.id}
+          text={draggedItem.text}
+        />
+      )}
+    </CustomDragLayerContainer>
+  ) : null;
+};
---
+++ client/src/state/AppStateContext.tsx
@@ -0,0 +1,72 @@
+import { createContext, useContext, FC, ReactNode, Dispatch } from "react";
+import { Action } from "./actions";
+import { appStateReducer } from "./appStateReducer";
+import { useImmerReducer } from "use-immer";
+import { DragItem } from "../components/DragItem";
+type Task = {
+  id: string;
+  text: string;
+};
+
+type List = {
+  id: string;
+  text: string;
+  tasks: Task[];
+};
+
+export type AppState = {
+  lists: List[];
+  draggedItem: DragItem | null;
+};
+
+const appData: AppState = {
+  draggedItem: null,
+  lists: [
+    {
+      id: "0",
+      text: "To Do",
+      tasks: [{ id: "c0", text: "Be Awesome At Typescript" }],
+    },
+    {
+      id: "1",
+      text: "In Progress",
+      tasks: [{ id: "c2", text: "Learn TypeScript" }],
+    },
+    {
+      id: "2",
+      text: "Done",
+      tasks: [{ id: "c3", text: "Begin to use Static Typing!" }],
+    },
+  ],
+};
+
+type AppStateContextProps = {
+  draggedItem: DragItem | null;
+  lists: List[];
+  getTasksByListId(id: string): Task[];
+  children?: ReactNode;
+  dispatch: Dispatch<Action>;
+};
+
+const AppStateContext = createContext<AppStateContextProps>(
+  {} as AppStateContextProps
+);
+
+export const AppStateProvider: FC<AppStateContextProps> = ({ children }) => {
+  const [state, dispatch] = useImmerReducer(appStateReducer, appData);
+  const { lists, draggedItem } = state;
+
+  const getTasksByListId = (id: string) => {
+    return lists.find((list) => list.id === id)?.tasks || [];
+  };
+  return (
+    <AppStateContext.Provider
+      value={{ draggedItem, lists, getTasksByListId, dispatch }}>
+      {children}
+    </AppStateContext.Provider>
+  );
+};
+
+export const useAppState = () => {
+  return useContext(AppStateContext);
+};
