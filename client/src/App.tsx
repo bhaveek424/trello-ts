356dd5db79dd9358d84d5d6f738190174cb81f53
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-12-19 03:43:28 +0530

    remove gitignore files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,15 +9,17 @@ import { CustomDragLayer } from "./components/CustomDragLayer";
 export const App = () => {
   const { lists, dispatch } = useAppState();
   return (
-    <AppContainer>
-      <CustomDragLayer />
-      {lists.map((list) => (
-        <Column text={list.text} id={list.id} key={list.id} />
-      ))}
-      <AddNewItem
-        toggleButtonText="+ Add another list"
-        onAdd={(text) => dispatch(addList(text))}
-      />
-    </AppContainer>
+    <>
+      <AppContainer>
+        <CustomDragLayer />
+        {lists.map((list) => (
+          <Column text={list.text} id={list.id} key={list.id} />
+        ))}
+        <AddNewItem
+          toggleButtonText="+ Add another list"
+          onAdd={(text) => dispatch(addList(text))}
+        />
+      </AppContainer>
+    </>
   );
 };
